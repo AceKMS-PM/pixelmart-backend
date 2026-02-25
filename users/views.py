08757ffde30a8117e7428424b54d80f77541475f
adoumasseo <adoumasseo@gmail.com>
2026-02-25 21:18:47 +0100

    [refactor]: Remove OutstandingToken from user/views.py
    Just a minor correction, Trying to get a grasp of the whole thing

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken, OutstandingToken
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
 
 from .serializers import (
